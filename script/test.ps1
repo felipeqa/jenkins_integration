f043868d766c079d4a992eb38af1883b27742085
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-14 01:48:34 -0300

    fix powershell

--- script/test.ps1
+++ script/test.ps1
@@ -1,3 +1,2 @@
-#docker run -v $(pwd):/tmp -e ENV=${ENV} -e SLACK=${SLACK} -e MY_PROFILE=${MY_PROFILE} -t product-editor-acceptance-tests $@
-docker run -v "${pwd}":/opt/jenkins -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
+docker run -v "${pwd}:/opt/jenkins" -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
 docker rm container-ruby
