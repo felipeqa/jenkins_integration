45c2d526c7fcae1f1904c7bfc2a51a2fcba68ced
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-15 16:25:59 -0300

    add param

--- script/test.ps1
+++ script/test.ps1
@@ -1,3 +1,7 @@
+Param(
+  [string]$BROWSER,
+  [string]$TAG
+)
 docker run -v "$(pwd):/opt/jenkins" -e BROWSER=$BROWSER -e TAG=$TAG -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
 docker rm -f container-ruby
 docker rm -f selenium-hub
