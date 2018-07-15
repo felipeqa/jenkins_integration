23d3fce7af70cf775a7ff47bf1bc5e60618c4146
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-15 15:07:57 -0300

    fix powershell

--- script/test.ps1
+++ script/test.ps1
@@ -1,3 +1,3 @@
-docker run -v "$(pwd):/opt/jenkins" -e BROWSER=remote -e TAG=backend -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
+docker run -v "$(pwd):/opt/jenkins" -e BROWSER=${BROWSER} -e TAG=${TAG} -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
 docker rm container-ruby
 docker rm selenium-hub
