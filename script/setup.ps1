d93abf115b553b0d6889f3fc40584adecc9ea405
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-15 15:10:16 -0300

    remove container before or after test

--- script/setup.ps1
+++ script/setup.ps1
@@ -1,2 +1,4 @@
+docker rm -f container-ruby
+docker rm -f selenium-hub
 docker run -d -p 4444:4444 --name selenium-hub selenium/standalone-chrome
 docker build -t cucumber/cucumber .
--- script/test.ps1
+++ script/test.ps1
@@ -1,3 +1,3 @@
 docker run -v "$(pwd):/opt/jenkins" -e BROWSER=${BROWSER} -e TAG=${TAG} -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
-docker rm container-ruby
-docker rm selenium-hub
+docker rm -f container-ruby
+docker rm -f selenium-hub
