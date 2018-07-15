75defa9e3cd5fcc9e9a05f44ce751459ea5d35ef
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-15 15:17:18 -0300

    fix variables

--- script/test.ps1
+++ script/test.ps1
@@ -1,3 +1,3 @@
-docker run -v "$(pwd):/opt/jenkins" -e BROWSER=${BROWSER} -e TAG=${TAG} -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
+docker run -v "$(pwd):/opt/jenkins" -e BROWSER=$BROWSER -e TAG=$TAG -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
 docker rm -f container-ruby
 docker rm -f selenium-hub
