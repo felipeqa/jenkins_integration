721ffc81f9540bbf182d2158cdd79dc99745d1db
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-07-15 19:48:56 -0300

    add pull standalone/selenium-chrome

--- script/setup.ps1
+++ script/setup.ps1
@@ -1,4 +1,5 @@
 docker rm -f container-ruby
 docker rm -f selenium-hub
+docker pull  selenium/standalone-chrome
 docker run -d -p 4444:4444 --name selenium-hub selenium/standalone-chrome
 docker build -t cucumber/cucumber .
